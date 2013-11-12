0e09659140b08672e7aecf46e96b0e1c48fcc08e
Patricia Gorla <gorla.patricia@gmail.com>
2013-11-11 23:08:59 -0500

    Write add/delete operations on keyspaces.

--- lilkv/keyspace.py
+++ lilkv/keyspace.py
@@ -6,20 +6,35 @@
     This module object contains all column families.
 """
 
+import sys
+
+from columnfamily import ColumnFamily
+
+KEYSPACES = {}
 
 class Keyspace(object):
     """Keyspaces contain a list of column families, along with key
     configuration details.
     """
-    KEYSPACES = {}
 
     def __init__(self, name):
+        self.name = name
         KEYSPACES[name] = self
         self.columnfamilies = {}
 
-    def add_columnfamily(self, cf):
+    def create_columnfamily(self, cf_name):
+        cf = ColumnFamily(cf_name)
         self.columnfamilies[cf.name] = cf
+        return cf
+
+    def delete_columnfamily(self, cf_name, sure=False):
+        if sure:
+            del self.columnfamilies[cf_name]
+            sys.stdout.write("ColumnFamily %s deleted." % cf_name)
+            sys.stdout.flush()
+        return
 
     def __repr__(self):
-        return '''Keyspace: %s % self.name
-            '''
+        return '''Keyspace: %s\n
+            \rColumnFamilies: %s\n
+            ''' % (self.name, self.columnfamilies)
