157c1748a33edad0ae90db24ae7308ff6ecf18b2
Patricia Gorla <gorla.patricia@gmail.com>
2013-11-11 23:30:00 -0500

    Separate row_key and col_name.
    D'oh.

--- lilkv/column.py
+++ lilkv/column.py
@@ -14,19 +14,21 @@ class Column(object):
     """The Column object is the fundamental data storage unit.
     Each Column contains a key, a value, a timestamp, and a TTL.
 
-    :param key:
+    :param row_key:
+    :param col_name:
     :param value:
     :param timestamp:
     :param TTL:
     """
 
-    def __init__(self, key, value=None, timestamp=None, 
-                 TTL=None, tombstone=False):
+    def __init__(self, row_key, col_name=None, value=None,
+                 timestamp=None, TTL=None, tombstone=False):
         self.timestamp = datetime.now() if timestamp is None else timestamp
-        self.key = key
+        self.row_key = row_key
+        self.col_name = col_name
         self.value = value
         self.TTL = TTL
         self.tombstone = tombstone
 
     def __repr__(self):
-        return '%s => %s' % (self.key, self.value)
+        return '%s => %s' % (self.row_key, (self.col_name, self.value))
